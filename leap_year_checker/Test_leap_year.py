466049381549a007cb9d951e4a4aa182f9ee6994
AmirMahdiAsadi556 <xxx3pal@gmail.com>
2025-03-01 06:35:10 +0100

    Update Test_leap_year.py

--- leap_year_checker/Test_leap_year.py
+++ leap_year_checker/Test_leap_year.py
@@ -5,6 +5,7 @@ print("welcome, I am Amir Mahdi")
 year = int(input("Please enter a year: "))
 
 if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    #f-string: اجازه می‌دهد تا مستقیم داخل متن متغیر قرار دهیم
     print(f"Yes, The year {year} is a leap year. :) ")
 else:
     print(f"No, the year {year} is not a leap year. :( ") 
