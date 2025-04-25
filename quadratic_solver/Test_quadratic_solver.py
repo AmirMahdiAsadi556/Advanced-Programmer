6125096d06fc32f3ebda15ab8354b463d407c5f0
AmirMahdiAsadi556 <xxx3pal@gmail.com>
2025-04-25 15:01:29 +0330

    Update Test_quadratic_solver.py

--- quadratic_solver/Test_quadratic_solver.py
+++ quadratic_solver/Test_quadratic_solver.py
@@ -4,23 +4,13 @@ print("welcome, I am Amir Mahdi")
 
 import math
 
-#نمایش میده برنماه چیکار میکنه
-
 print("This program solves a quadratic equation: ax² + bx + c = 0")
 
-#ضرایب رو دریافت میکنیم
 a = float(input("Enter coefficient a: "))
 b = float(input("Enter coefficient b: "))
 c = float(input("Enter coefficient c: "))
-
-# Calculating the discriminant
 # محاسبه مقدار دلتا (D)
-
 D = b**2 - 4*a*c
-
-# Checking the nature of the roots
-# بررسی نوع ریشه‌ها و محاسبه مقدار آن‌ها
-
 #معادله دارای دو ریشه حقیقی است
 if D > 0:
     root1 = (-b + math.sqrt(D)) / (2*a)
@@ -32,4 +22,4 @@ elif D == 0:
     print(f"The equation has one real root: {root:.2f}")
 #معادله دارای ریشه حقیقی نیست   
 else:
-    print("The equation has no real roots (complex roots).")
+    print("The equation has no real roots.")
